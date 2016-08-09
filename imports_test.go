9ebfd00158beb81c058fcf2192afcb5779a38ba3
Kevin Deenanauth <kevin.deenanauth@rosetta.com>
2016-08-09 14:36:47 -0700

    increase imports test coverage

--- imports_test.go
+++ imports_test.go
@@ -25,3 +25,22 @@ func (f *ImportsSuite) TestUnqualifiedImport(c *C) {
 	actual := imp.getQualifier()
 	c.Assert(actual, Equals, expected)
 }
+
+func (f *ImportsSuite) TestAliasedImport(c *C) {
+	expected := "blah."
+	imp := &ImportSpec{
+		Package:   "bytes",
+		Qualified: true,
+		Alias:     "blah",
+	}
+	actual := imp.getQualifier()
+	c.Assert(imp.GetAlias(), Equals, "blah")
+	c.Assert(imp.GetPackage(), Equals, "bytes")
+	c.Assert(actual, Equals, expected)
+}
+
+func (f *ImportsSuite) TestNils(c *C) {
+	nilInst := ((*ImportSpec)(nil))
+	c.Assert(nilInst.GetAlias(), Equals, "")
+	c.Assert(nilInst.GetPackage(), Equals, "")
+}

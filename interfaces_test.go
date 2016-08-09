8db526b3017d29c3663cb09cb0f4fe1bd93043e1
Kevin Deenanauth <kevin.deenanauth@rosetta.com>
2016-08-09 14:17:47 -0700

    function tests

---
+++ interfaces_test.go
@@ -0,0 +1,45 @@
+package gopoet
+
+import . "gopkg.in/check.v1"
+
+type InterfaceSuite struct{}
+
+var _ = Suite(&InterfaceSuite{})
+
+func (f *InterfaceSuite) TestBlankInterface(c *C) {
+	expected := "type NewInterface interface {\n" +
+		"}\n"
+
+	i := NewInterfaceSpec("NewInterface")
+	actual := i.String()
+	c.Assert(actual, Equals, expected)
+}
+
+func (f *InterfaceSuite) TestBlankInterfaceWithComment(c *C) {
+	expected := "// NewInterface is a cool interface\n" +
+		"type NewInterface interface {\n" +
+		"}\n"
+
+	i := InterfaceSpec{Name: "NewInterface", Comment: "NewInterface is a cool interface"}
+	actual := i.String()
+	c.Assert(actual, Equals, expected)
+}
+
+func (f *InterfaceSuite) TestInterfaceWithMethods(c *C) {
+	expected := "type NewInterface interface {\n" +
+		"\tTestA(paramA string)\n" +
+		"\tTestB()\n" +
+		"}\n"
+
+	i := NewInterfaceSpec("NewInterface")
+	i.
+		Method(
+			NewFuncSpec("TestA").
+				FunctionComment("TestA does stuff").
+				Parameter("paramA", TypeReferenceFromInstance("")),
+		).
+		Method(NewFuncSpec("TestB"))
+
+	actual := i.String()
+	c.Assert(actual, Equals, expected)
+}
